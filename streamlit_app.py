f356d7176f007e5d43c8587ecbad783afe07fd7c
kgujral1105 <kgujral@deloitte.com>
2025-01-06 13:21:40 -0800

    Update streamlit_app.py
    Testing the dataframe conversion to Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import Pandas as py
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -32,8 +33,15 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 #my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Convert snowpark dataframe to Pandas Dataframe so we can use the LOC function
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
 
 ingredients_list =  st.multiselect('Choose upto 5 ingredients:', my_dataframe,max_selections=5)
 
